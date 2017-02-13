3465925cf09be9820d66399bed953d054ff88d61
Pablo Donato <pablo.donato@mailoo.org>
2017-02-13 21:51:58 +0100

    Pretty color print in test.py

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 from time import sleep
 from gym_numgrid.envs import NumGrid
 
+red = '\033[91m'
+yellow = '\033[93m'
+green = '\033[32m'
+endc = '\033[0m'
+
 env = NumGrid()
 env.configure(size=(5,5), render_scale=4, draw_grid=True)
 for i_episode in range(20):
@@ -11,14 +16,13 @@ for i_episode in range(20):
         digit = env.digit_space.sample()
         pos = env.cursor_move(env.direction_space.sample(), distance=1)
         action = (digit, tuple(pos))
-        print('action:', action)
+        color = ''
         observation, reward, done, info = env.step(action)
         if info['out_of_bounds']:
-            print("Can't get out of the world!")
-        if info['digit'] is not None:
-            print("digit:", info['digit'])
-            if digit == info['digit']:
-                print("You found the right digit!")
+            print(yellow + "Can't get out of the world!" + endc)
+        if digit != 10:
+            color = green if digit == info['digit'] else red
+        print(color + 'action:', action, endc)
         if done:
             print("Episode finished after {} timesteps".format(t+1))
             break
