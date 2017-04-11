da56d4771d81329d95e4e494fd11c77ddc42ee2c
Pablo Donato <pablo.donato@mailoo.org>
2017-04-11 16:20:52 +0200

    Add attributes to NumGridWrapper

--- gym_numgrid/wrappers/numgrid.py
+++ gym_numgrid/wrappers/numgrid.py
@@ -4,7 +4,7 @@ from gym_numgrid.envs import NumGrid
 
 class NumGridWrapper(gym.Wrapper):
     """
-    Base class for NumGrid wrappers conserving additional spaces.
+    Base class for NumGrid wrappers conserving additional spaces and attributes.
     """
     def __init__(self, env):
         if not (isinstance(env, NumGrid) or isinstance(env, NumGridWrapper)):
@@ -13,3 +13,6 @@ class NumGridWrapper(gym.Wrapper):
 
         self.digit_space = self.env.digit_space
         self.position_space = self.env.position_space
+
+        self.cursor_size = self.env.cursor_size
+        self.num_steps = self.env.num_steps
